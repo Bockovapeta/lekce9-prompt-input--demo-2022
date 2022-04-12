611a8f2917f8bd86606f3cb099ff790cd0d17861
Petra Bočková <Wickky@seznam.cz>
2022-04-12 19:07:15 +0200

    BMI vypocet

---
+++ bmi-input.js
@@ -0,0 +1,22 @@
+// funkce pro výpočet BMI 
+
+function vypocitejBMI() {
+    let vyskaMetry = parseFloat(document.querySelector('input[name="vyska"]').value) / 100;
+    let vahaKilogramy = parseFloat(document.querySelector('input[name="vaha"]').value);
+
+    let BMI = Math.round(vahaKilogramy / Math.pow(vyskaMetry, 2));
+
+    vypisBMI(BMI);
+}
+
+
+
+//funkce pro zapsani BMI do UI (uživatelské rozhraní)
+
+function vypisBMI(BMI) {
+    document.querySelector('#vysledek>span').innerHTML = BMI;
+}
+
+document.querySelectorAll('input').forEach((element) => {
+    element.addEventListener('change', vypocitejBMI);
+});

0312cf7fff5481620f75a067193f88c3a384d5b7
Petra Bočková <Wickky@seznam.cz>
2022-04-12 18:32:02 +0200

    implement prompt

---
+++ prompt.js
@@ -0,0 +1,9 @@
+// vytvoření promptu pro zadani jmena + prijmeni a veku a napldeni do proměných
+let celeJmeno = prompt('Zadej jméno a příjmení');
+let vek = prompt('Zadej svůj věk', 18);
+
+// vypsání výsledku pomocí alert
+// obracené uvozovky se dělají pomocí Alt+96s
+alert(`Jmenuješ se ${celeJmeno} a je ti ${vek} let.`);
+
+
